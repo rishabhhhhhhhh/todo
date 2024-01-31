cc6dda0094597d6bdb2b3f296981bef3d18f068c
rishabhhhhhhhh <p.rishabh.arora@gmail.com>
2024-02-01 03:55:07 +0530

    fix url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
 // export const SERVER_URL = "http://localhost:3001";
-export const SERVER_URL = "https://todo-server-24j2.onrender.com";
+export const SERVER_URL = "https://todo-server-rosy.vercel.app";
 
 export const STATUS = [
   { value: "To Do", label: "To Do" },
